97f2c6df9eeafbe6f360391605776219a7cde2bf
Shakir Mairaj Mufti <shakir.mufti@gmail.com>
2023-12-03 23:04:34 +0530

    Delete EndtoEndTesting directory

--- EndtoEndTesting/myfirsttest.cy.js
+++
@@ -1,9 +0,0 @@
-describe('My First Test', () => {
-    it('test1', () => {
-
-        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-
-        cy.title().should('eq','OrangeHRM')
-      
-    })
-  })
